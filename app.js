a0b5d455a5240917cd84488fad5b9eee8eb2bd63
Fred Cheng <fredcheng@cal.berkeley.edu>
2024-03-12 12:15:35 -0700

    Review 3 bug fix

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 require("dotenv").config();
 
+// must have require lines first, or else it will cause a linter error - per reviewer
+
+// External requirements
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const { PORT = 3001 } = process.env;
 
+// Internal Requirements
 // Sprint 15.2.5. Handle Errors
 const { errors } = require("celebrate");
-
 // Sprint 15.1. Create New Middleware
 const errorHandler = require("./middlewares/errorHandler");
-
 // Sprint 15.3. Logger
 const { requestLogger, errorLogger } = require("./middlewares/logger");
-
 // celebrate needed to be imported before routes
 const routes = require("./routes");
 
-
+// start code here.  After all the require statments.
+const { PORT = 3001 } = process.env;
 const app = express();
 app.use(cors());
 
